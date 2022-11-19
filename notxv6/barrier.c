63eafadd8936f2d91aad53fe06816aeac17f786f
TomFish <1658047827@qq.com>
2022-11-19 11:04:18 +0800

    change comment

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -38,12 +38,15 @@ barrier()
     // 所有线程到达屏障
     bstate.round++;
     bstate.nthread = 0;  
-    // 以上这两个变量更新需要放在前，避免某些thread太快进入下一轮时
-    // 这里两个变量还没修改，触发assert
-    // 或者错误地更新bstate.nthread，最终导致所有线程永远无法继续执行
+    // 当最后一个线程broadcast之后，其他的线程从wait醒来竞争锁，但是实际上
+    // 锁被当前这个broadcast的线程占着，其他线程均无法得到锁
+    // 只有当这个线程完成所有变量的修改，主动释放锁，其他的线程才能抢得到锁
+    // 这保证了出现某线程在其他线程退出barrier之前进入了下一轮循环不会出错
+    // 因为某线程能退出barrier的前提是broadcast的那个线程已经完成所有变量的更新
+    // 之后过快进入下一轮的线程nthread++是合法的
     pthread_cond_broadcast(&bstate.barrier_cond);
   }else{
-    // 还有线程没到，当前线程锁，阻塞
+    // 还有线程没到，当前线程：放锁+阻塞
     pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
   }
 
